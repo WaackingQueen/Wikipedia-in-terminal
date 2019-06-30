7db1d1c34516f8aa2ac45b0478d3507affcf8035
lume <lume@itu.dk>
2019-06-30 11:18:36 +0200

    New version of Wikipedia-in-Terminal

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Wikipedia-in-terminal
 This program will give you a quick summary from Wikipedia on your requested topic.
 
+Usage: './wiki.py "Search term" [no. sentences]'
+
 Note! The program will only display the first 10 lines from
-the Wikipedia summary. For more information, click the link below the summary.
+the Wikipedia summary as a default, if no integer is entered. Type an integer in the end of your argument for desired number of sentences.
--- wiki.py
+++ wiki.py
@@ -1,12 +1,27 @@
+#!/usr/bin/python
+
 import wikipedia
+import sys, getopt
 
-def search():
-    topic = raw_input("Search Wikipedia: ")
-    a = wikipedia.page(topic)
-    b = a.url
-    print wikipedia.summary(topic, sentences = 10)
-    print ('''
-    For more information click: 
-    ''') + b
+def main():
+   #If there are no arguments 
+   if len(sys.argv) == 1:
+      print 'USAGE: ./wiki.py "Search term" [no. sentences]'
+      return
 
-search()
+   #Default # of sentences if the only argument is 'topic' 
+   topic = sys.argv[1]
+   sentences = 10
+
+   if len(sys.argv) > 2:
+      sentences = int(sys.argv[2])
+
+   print wikipedia.summary(topic, sentences = sentences)
+   a = wikipedia.page(topic)
+   b = a.url
+   print ('''
+   For more information click: 
+   ''') + b
+
+if __name__ == "__main__":
+   main()
